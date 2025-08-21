af64e0c97382df1e635e4533cbcece6defffb722
Abhijay Sharma <abhijaysharma0225@gmail.com>
2025-08-21 11:50:44 +0530

    adding Lead created time in django admin dahsboard

--- leads/admin.py
+++ leads/admin.py
@@ -8,3 +8,10 @@ admin.site.register(UserProfile)
 admin.site.register(Lead)
 admin.site.register(Agent)
 admin.site.register(Category)
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'emails', 'phone_number', 'created_time')
+    list_filter = ('created_time',)
+    ordering = ('-created_time',)
+
